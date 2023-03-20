2f632dd297f643f35a4a9170d444d064b4583e3c
Matt Scott <matt@azorian.solutions>
2023-03-20 16:03:24 -0400

    Working on getting the `pda run` command working right with the various HTTP servers with venv support.

--- src/lib/cli/cmd/cmd_run.py
+++ src/lib/cli/cmd/cmd_run.py
@@ -20,30 +20,39 @@ def cli(ctx: Environment):
             "forget to activate a virtual environment?"
         ) from exc
 
-    command: list
+    command: list = []
+
+    # If a virtual environment is enabled, activate it before running the server
+    # if ctx.settings.venv_enabled and isinstance(ctx.settings.venv_path, str) and len(ctx.settings.venv_path.strip()):
+    #     venv_path: str = ctx.settings.venv_path
+    #     # If the virtual environment path is not absolute, make it absolute by prepending the root path
+    #     if not os.path.isabs(venv_path):
+    #         venv_path = os.path.join(ctx.settings.root_path, venv_path)
+    #     command += ['source', f'{venv_path}/bin/activate', ';']
 
     # Run the application with Gunicorn HTTP/WSGI server
     if ctx.settings.server_type == 'gunicorn':
-        server_args: list = ['--workers=3', '--threads=3', '--timeout=0', '--log-level=info',
-                             '--log-file=-', '--access-logfile=-', '--error-logfile=-', '--enable-stdio-inheritance',
-                             '--capture-output', f'--bind={ctx.settings.server_address}:{ctx.settings.server_port}']
+        command += ['gunicorn', 'src.pda.wsgi']
+        command_args: list = ['--workers=3', '--threads=3', '--timeout=0', '--log-level=info',
+                              '--log-file=-', '--access-logfile=-', '--error-logfile=-', '--enable-stdio-inheritance',
+                              '--capture-output', f'--bind={ctx.settings.server_address}:{ctx.settings.server_port}']
         server_env = os.environ.copy()
-        server_env['GUNICORN_CMD_ARGS'] = ' '.join(server_args)
-        subprocess.run(['gunicorn', 'src.pda.wsgi'], env=server_env)
+        server_env['GUNICORN_CMD_ARGS'] = ' '.join(command_args)
+        subprocess.run(command, env=server_env)
 
     # Run the application with Uvicorn ASGI server
     elif ctx.settings.server_type == 'uvicorn':
-        server_args: list = ['uvicorn', '--host', ctx.settings.server_address, '--port', str(ctx.settings.server_port),
-                             '--log-level', 'info', '--access-log', '--workers', '3', '--proxy-headers',
-                             'src.pda.asgi:application']
-        subprocess.run(server_args)
+        command += ['uvicorn', '--host', ctx.settings.server_address, '--port', str(ctx.settings.server_port),
+                    '--log-level', 'info', '--access-log', '--workers', '3', '--proxy-headers',
+                    'src.pda.asgi:application']
+        subprocess.run(command)
 
     # Run the application with UWSGI server
     elif ctx.settings.server_type == 'uwsgi':
-        server_args: list = ['uwsgi', '--http-socket', f'{ctx.settings.server_address}:{ctx.settings.server_port}',
-                             '--workers', '3', '--threads', '3', '--enable-threads', '--plugins', 'python3',
-                             '--wsgi-file', 'src/pda/wsgi.py']
-        subprocess.run(server_args)
+        command += ['uwsgi', '--http-socket', f'{ctx.settings.server_address}:{ctx.settings.server_port}',
+                    '--workers', '3', '--threads', '3', '--enable-threads', '--plugins', 'python3',
+                    '--wsgi-file', 'src/pda/wsgi.py']
+        subprocess.run(command)
 
     # Run the application with Django built-in HTTP server for all unmatched server types
     else:
