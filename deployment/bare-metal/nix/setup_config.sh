bff27c811d165f02ffef934a847a47da535e9688
Matt Scott <matt@azorian.solutions>
2023-03-19 23:35:09 -0400

    Working on deployment scripts for the Linux platform.

--- deployment/bare-metal/nix/setup_config.sh
+++ deployment/bare-metal/nix/setup_config.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+# Create an empty placeholder file for the yaml config file if it does not exist
+touch "$PDA_CONFIG_PATH"
+
 # Generate a new secret_key setting value
 # shellcheck disable=SC2034
 PDA_SECRET_KEY=$(pda gen_salt -r)
 
 # Add the secret_key setting to the list of environment variables that have been set
 PDACLI_ENV_VARS_SET+=("PDA_SECRET_KEY")
-
-touch "$PDA_CONFIG_PATH"
 
 # Create a variable to hold the environment variable settings that will be saved to the .env.dev file
 env_data=''
