06cd35d75b07333ec7df312addceb47a94f11ce9
Matt Scott <matt@azorian.solutions>
2024-02-05 08:03:31 -0500

    Updated the `account.AccountInvitation` model to use a status field with static status definitions in place of the multiple boolean flag fields.

--- src/apps/account/models.py
+++ src/apps/account/models.py
@@ -47,19 +47,21 @@ class AccountUser(models.Model):
 class AccountInvitation(models.Model):
     from apps.user.models import User
 
+    STATUS_DRAFT = 'draft'
+    STATUS_PENDING = 'pending'
+    STATUS_ACCEPTED = 'accepted'
+    STATUS_REJECTED = 'rejected'
+    STATUS_CANCELED = 'canceled'
+    STATUS_EXPIRED = 'expired'
+    STATUS_INVALID = 'invalid'
+    STATUS_DELETED = 'deleted'
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='accountinvitation_user')
     email = models.EmailField()
     role = models.CharField(max_length=30, default=AccountUser.ROLE_USER)
-    is_admin = models.BooleanField(default=False)
-    is_pending = models.BooleanField(default=True)
-    is_accepted = models.BooleanField(default=False)
-    is_rejected = models.BooleanField(default=False)
-    is_canceled = models.BooleanField(default=False)
-    is_expired = models.BooleanField(default=False)
-    is_invalid = models.BooleanField(default=False)
-    is_deleted = models.BooleanField(default=False)
+    status = models.CharField(max_length=30, default=STATUS_DRAFT)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
                                    related_name='accountinvitation_created_by')
     created_at = models.DateTimeField(auto_now_add=True)
